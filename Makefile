9c7776f1ec1b4e62b0227f16724d2a015c4eceaa
Leonardo Costa Santos <leonardo_c_santos@usp.br>
2020-10-01 10:43:59 -0300

    adiciona prototipo de escalonador FCFS

--- Makefile
+++ Makefile
@@ -1,11 +1,13 @@
+CFLAGS = -g -Wall
+
 default: bccsh ep1
 
 
 bccsh:
-	gcc -g bccsh.c -o bccsh -lreadline -lhistory
+	gcc $(CFLAGS) bccsh.c -o bccsh -lreadline -lhistory
 
 ep1: ep1.c
-	gcc -g ep1.c -o ep1 -pthread
+	gcc $(CFLAGS) ep1.c -o ep1 -pthread
 
 clean:
 	rm -f ep1 ep1.o bccsh bccsh.o
--- ep1.c
+++ ep1.c
@@ -2,8 +2,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <time.h>
 
 #define MAX_LINE_LENGTH 100
+
+static int debug = 0;
 
 // Struct to store data about the simulated processes
 typedef struct {
@@ -19,6 +22,15 @@ typedef struct proc_node {
     pthread_t thread;
     struct proc_node *next;
 } Process;
+
+void printProcessData(FILE *file, ProcessData *data) {
+    fprintf(file,
+            "%s %d %d %d",
+            data->name,
+            data->t0,
+            data->dt,
+            data->deadline);
+}
 
 // Reads trace file, returns linked list with process data
 Process* get_processes(FILE *trace) {
@@ -57,53 +69,96 @@ Process* get_processes(FILE *trace) {
 void* work(void* data) {
     ProcessData *pd = (ProcessData*) data;
 
-    return (void *) pd->name;
+    sleep(pd->dt);
+
+    return NULL;
+}
+
+void fcfs_scheduler(Process *process_list, FILE *output_file) {
+    /*
+     * inicial_t: time at beginning of simulation (t == 0);
+     * t: current time
+     * t0: time current process started, relative to beginning of simulation
+     */
+    struct timespec inicial_t, t, t0;
+    int tf, tr;
+
+    Process *current = process_list->next;
+
+    clock_gettime(CLOCK_REALTIME, &inicial_t);
+
+    while (current) {
+        if (debug) {
+            fprintf(stderr, "new process arrived: ");
+            printProcessData(stderr, current->data);
+            fprintf(stderr, "\n");
+        }
+
+        pthread_create(&(current->thread), NULL, work, current->data);
+        clock_gettime(CLOCK_REALTIME, &t0);
+
+        if (pthread_join(current->thread, NULL)) {
+            fprintf(stderr, "failed to join thread %s\n", current->data->name);
+        }
+        clock_gettime(CLOCK_REALTIME, &t);
+
+        tf = t.tv_sec - inicial_t.tv_sec;
+        tr = t.tv_sec - t0.tv_sec;
+
+        if (debug) {
+            fprintf(stderr, "process ended: ");
+            fprintf(stderr, "%s %d %d\n", current->data->name, tf, tr);
+        }
+
+        fprintf(output_file, "%s %d %d\n", current->data->name, tf, tr);
+
+        process_list->next = current->next;
+        free(current->data);
+        free(current);
+        current = process_list->next;
+    }
+
+    free(process_list);
 }
 
 int main(int argc, char **argv) {
     int scheduler;
     FILE *trace, *output;
     char *output_file;
-    Process *process_list, *current, *aux;
+    Process *process_list;
 
     if (argc < 4) {
-        printf("Usage: %s <scheduler_id> <trace_file> <output_file>", argv[0]);
+        printf("Usage: %s <scheduler_id> <trace_file> <output_file>\n", argv[0]);
         return -1;
+    }
+
+    if (argc == 5 && argv[4][0] == 'd') {
+        debug = 1;
+        printf("debug mode active\n");
     }
 
     scheduler = atoi(argv[1]);
+
     if ((trace = fopen(argv[2], "r")) == 0) {
-        printf("Error: Failed to open trace file.\nExiting ...\n");
+        printf("error: failed to open trace file.\nExiting ...\n");
         return -1;
     }
-    output_file = argv[3];
-
     process_list = get_processes(trace);
     fclose(trace);
 
-    // create all threads in process list
-    current = process_list->next;
-    while (current != NULL) {
-        pthread_create(&(current->thread), NULL, work, current->data);
-        current = current->next;
-    }
+    output_file = argv[3];
 
     output = fopen(output_file, "w");
 
-    current = process_list->next;
-    while (current != NULL) {
-        char *name;
-
-        pthread_join(current->thread, (void*) &name);
-        fprintf(output, "o processo '%s' terminou\n", name);
-        name = NULL;
-        
-        free(current->data);
-        aux = current;
-        current = current->next;
-        free(aux);
+    switch (scheduler) {
+        case 1:
+            fcfs_scheduler(process_list, output);
+            break;
+        default:
+            fprintf(stderr, "error: invalid scheduler value\n");
     }
-    free(process_list);
+    process_list = NULL;
+
     fclose(output);
 
     return 0;
