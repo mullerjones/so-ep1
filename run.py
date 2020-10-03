b42007e6e8da09cee5d6075e22d180abc2bfbd54
mullerjones <ale.mullerjones@gmail.com>
2020-10-03 19:19:15 -0300

    testes e testador

--- run.py
+++ run.py
@@ -1,14 +1,14 @@
 import os
 
-for i in range(1, 11):
-    cmd = './ep1 1 small_test' + str(i) + '.txt output.txt'
+for i in range(1, 31):
+    cmd = './ep1 1 small_test1.txt output.txt'
     os.system(cmd)
     os.rename('output.txt','output_small' +str(i)+ '.txt')
     
-    cmd = './ep1 1 medium_test' + str(i) + '.txt output.txt'
+    cmd = './ep1teste 1 medium_test1.txt output.txt'
     os.system(cmd)
     os.rename('output.txt','output_medium' +str(i)+ '.txt')
     
-    cmd = './ep1 1 large_test' + str(i) + '.txt output.txt'
+    cmd = './ep1teste 1 large_test1.txt output.txt'
     os.system(cmd)
     os.rename('output.txt','output_large' +str(i)+ '.txt')
